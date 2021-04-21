6215a326801fe4003dfd40c37e68f1c78b50b033
Vitaly Kuprin <vitas_s@inbox.lv>
2021-04-21 08:55:43 -0700

    Add User model spec

--- components/chat/spec/models/group_spec.rb
+++ components/chat/spec/models/group_spec.rb
@@ -9,5 +9,6 @@ RSpec.describe Chat::GroupMessage, type: :model do
 
   context "Validation tests" do
     it { should validate_presence_of(:message) }
+    it { should validate_presence_of(:user_id) }
   end
 end
